d04f722bd725533bb6c1355438a09095eeb83a95
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-10-28 16:03:58 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Search_Algorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of elements in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0)) && (n <= 20))
+                        break;
+                else
+                    Console.
+            }
+        }
+    }
+}
